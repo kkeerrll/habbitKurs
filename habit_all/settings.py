23b47a2c9da8e0bee6e5bafb46ce1e5aff4db9a4
anzelikagudkova <kkeerrll137@gmail.com>
2023-12-25 10:28:55 +0300

    docker+

--- habit_all/settings.py
+++ habit_all/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from datetime import timedelta
-
+from urllib.parse import urlsplit
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(4_g0_09llq#pkn*yq-0m&!9q8r1cx4g(8-7v*9m^p_qvf!l$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [urlsplit(DOMAIN_NAME).hostname]
 
 DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'habit_all.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASES_NAME'),
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DATABASES_PASSWORD'),
-        'HOST': 'localhost',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432',
     }
 }
@@ -181,8 +181,8 @@ CACHES = {
         'LOCATION': os.getenv('CACHES_LOCATION'), }}
 
 # Celery broker settings
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_HOST')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_BROKER_HOST')
 
 CELERY_TASK_TRACK_STARTED = True
 # CELERY_TASK_TIME_LIMIT = 30 * 60
